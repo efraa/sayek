832f37c3304adc776aa48ec02e8467707ebfa966
Efra Peralta <efra23peralta@gmail.com>
2020-07-22 20:02:06 -0400

    feat: update access token with axios interceptors

--- src/api/apiConnection.ts
+++ src/api/apiConnection.ts
@@ -13,6 +13,30 @@ const API = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
   withCredentials: true,
 })
+
+// Request interceptor for API calls
 API.interceptors.request.use(requestOptions)
+
+// Response interceptor for API calls
+API.interceptors.response.use(
+  res => res,
+  async err => {
+    const req = err.config
+
+    if (
+      err.response?.status === 401 &&
+      req.url.includes('/users/refresh-token')
+    )
+      return Promise.reject(err)
+
+    if (err.response?.status === 401 && !req._retry) {
+      req._retry = true
+      await token.refresh()
+      return API(req)
+    }
+
+    return Promise.reject(err)
+  }
+)
 
 export { API }
--- src/api/token.ts
+++ src/api/token.ts
@@ -1,7 +1,11 @@
+import { AuthenticationService } from './services/AuthenticationService'
+
 let requestToken: string | undefined
 
 export const token = {
   get: () => requestToken,
   set: (token: string) => (requestToken = token),
   destroy: () => (requestToken = undefined),
+  refresh: async () =>
+    (requestToken = (await AuthenticationService.refreshToken()).data.token),
 }
