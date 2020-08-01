43f1d069f3a0e5e324933c167c1984ae8f51f4b5
Efra Peralta <efra23peralta@gmail.com>
2020-07-31 20:42:04 -0400

    feat: add 404 page

--- src/screens/NotFound/NotFound.tsx
+++ src/screens/NotFound/NotFound.tsx
@@ -1,14 +1,29 @@
 import React, { memo } from 'react'
 
 // Comps
+import Asset from '../../assets/404.svg'
 import { Container, Row, Col } from 'reactstrap'
+import { Screen, Image, Text } from './Style'
+import { Title } from '../../components/Common'
+import { Button } from '../../components/Button'
+import { Link } from 'react-router-dom'
 
 export const NotFound = memo(() => (
-  <Container>
-    <Row>
-      <Col xs={12}>
-        <h3>No encontrado</h3>
-      </Col>
-    </Row>
-  </Container>
+  <Screen>
+    <Container fluid>
+      <Row>
+        <Col xs={12}>
+          <Image src={Asset} alt="Not Found" />
+          <Title>¡Vaya! No podemos encontrar nada aquí.</Title>
+          <Text>
+            Lamentablemente, la página que está buscando se ha movido, eliminado
+            o nunca existió.
+          </Text>
+          <Button as={Link} to="/home">
+            Ir a Inicio
+          </Button>
+        </Col>
+      </Row>
+    </Container>
+  </Screen>
 ))
--- src/screens/NotFound/Style.tsx
+++ src/screens/NotFound/Style.tsx
@@ -1,5 +1,24 @@
 import styled from 'styled-components'
 
 export const Screen = styled.div`
+  padding: 32px 0;
+  min-height: 100vh;
   display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+`
+
+export const Image = styled.img`
+  width: 100%;
+  max-width: 400px;
+  margin-bottom: 24px;
+`
+
+export const Text = styled.p`
+  padding: 0;
+  margin: auto;
+  margin-bottom: 24px;
+  max-width: 450px;
+  line-height: 24px;
 `
