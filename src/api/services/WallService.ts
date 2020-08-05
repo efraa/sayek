21c9accd479359b112d75b40a08d466f9079048c
Efra Peralta <efra23peralta@gmail.com>
2020-08-05 19:33:23 -0400

    feat: update endpoints

--- src/api/services/WallService.ts
+++ src/api/services/WallService.ts
@@ -4,11 +4,11 @@ import { WallDTO } from '../dtos/WallDTO'
 
 export const WallService = {
   list: async (page: number | unknown): Promise<ListOfWallDTO> =>
-    (await API.get(`/walls/list/?page=${page}`)).data,
+    (await API.get(`/walls/?page=${page}`)).data,
 
   create: async (name: string): Promise<WallDTO> =>
     (await API.post('/walls/', { name })).data,
 
   get: async (id: number): Promise<WallDTO> =>
-    (await API.get(`/walls/get/${id}`)).data,
+    (await API.get(`/walls/${id}`)).data,
 }
