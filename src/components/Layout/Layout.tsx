950d438b1ed11069c4db602f4d207810fcf70d0e
Efra Peralta <efra23peralta@gmail.com>
2020-08-08 17:09:06 -0400

    perf: modify Layout Component to show different states on screens

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,18 +1,36 @@
-import React, { memo } from 'react'
+import React from 'react'
 import { Container, Row, Col } from 'reactstrap'
 import { Head } from '../Head'
+import NotFound from '../../screens/NotFound'
+import { Spinner } from '../Spinner'
 
 interface ILayout extends React.HTMLProps<HTMLDivElement> {
   title: string
+  show?: boolean
+  isLoading?: boolean
+  error?: boolean
 }
 
-export const Layout: React.FC<ILayout> = memo(({ title, children }) => (
-  <Container fluid>
-    <Row>
-      <Col xs={12}>
-        <Head title={title} />
-        {children}
-      </Col>
-    </Row>
-  </Container>
-))
+export const Layout: React.FC<ILayout> = ({
+  title,
+  children,
+  show = true,
+  isLoading = false,
+  error = false,
+}) =>
+  !error ? (
+    show && !isLoading ? (
+      <Container fluid>
+        <Row>
+          <Col xs={12}>
+            <Head title={title} />
+            {children}
+          </Col>
+        </Row>
+      </Container>
+    ) : (
+      <Spinner />
+    )
+  ) : (
+    <NotFound />
+  )
