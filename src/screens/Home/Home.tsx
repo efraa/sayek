c560326ff14629e2b815c53718b469d07b86babd
Efra Peralta <efra23peralta@gmail.com>
2020-08-08 17:12:25 -0400

    feat(HomeScreen): add user logged at home

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,7 +1,7 @@
-import React, { memo } from 'react'
+import React, { useContext } from 'react'
 import { Paths } from '../../routes/Paths'
+import { AuthContext } from '../../Authentication'
 
-// Comps
 import { Container, Row, Col } from 'reactstrap'
 import { Screen, Logout, UserHelp, Header } from './Style'
 import { Title } from '../../components/Common'
@@ -9,30 +9,36 @@ import { ComposeButton } from '../../components/ComposeButton'
 import { ActionButton } from '../../components/ActionButton'
 import { Messages } from '../../data/messageConstants'
 
-export const Home = memo(() => (
-  <Screen>
-    <Container fluid>
-      <Row>
-        <Col xs={12}>
-          <Header>
-            <UserHelp>
-              <span>{Messages.home.hello}</span>
-              <h4>Gryffindor 52</h4>
-              <small>{Messages.home.usernameHelp}</small>
-            </UserHelp>
-          </Header>
-          <ComposeButton to={Paths.walls.compose}>
-            {Messages.walls.create.title}
-          </ComposeButton>
-          <Title>{Messages.home.help}</Title>
-          {Messages.home.actions.map(({ title, id, to }) => (
-            <ActionButton key={id} to={to}>
-              {title}
-            </ActionButton>
-          ))}
-          <Logout to={Paths.logout}>{Messages.logout.title}</Logout>
-        </Col>
-      </Row>
-    </Container>
-  </Screen>
-))
+export const Home = () => {
+  const {
+    state: { user },
+  } = useContext(AuthContext)
+
+  return (
+    <Screen>
+      <Container fluid>
+        <Row>
+          <Col xs={12}>
+            <Header>
+              <UserHelp>
+                <span>{Messages.home.hello}</span>
+                <h4>{user?.username}</h4>
+                <small>{Messages.home.usernameHelp}</small>
+              </UserHelp>
+            </Header>
+            <ComposeButton to={Paths.walls.compose}>
+              {Messages.walls.create.title}
+            </ComposeButton>
+            <Title>{Messages.home.help}</Title>
+            {Messages.home.actions.map(({ title, id, to }) => (
+              <ActionButton key={id} to={to}>
+                {title}
+              </ActionButton>
+            ))}
+            <Logout to={Paths.logout}>{Messages.logout.title}</Logout>
+          </Col>
+        </Row>
+      </Container>
+    </Screen>
+  )
+}
